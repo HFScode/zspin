2eb90c7d5791d8a2f32567897a195069271bd8b1
godric <godric@0x3f.fr>
2014-12-12 01:01:25 +0100

    poc: select files in user dir

---
+++ sources/services/settings.js
@@ -0,0 +1,35 @@
+'use strict'
+
+var fs        = require('fs');
+var gui       = require('nw.gui');
+var mkdirp    = require('mkdirp');
+var iniparser = require('iniparser');
+var $path     = require('path');
+
+var app = angular.module('app');
+var APP_ROOT = $path.join(gui.App.dataPath, 'Zspin');
+var INI_ROOT = $path.join(APP_ROOT, 'Settings');
+mkdirp.sync(INI_ROOT);
+
+
+app.factory('settings', [ 
+  function () {
+    return {
+      // Set root paths
+      root: INI_ROOT,
+      appRoot: APP_ROOT,
+
+      // Actual ini parsing
+      parse: function(name) {
+        var filename = name + '.ini';
+        console.log('filename: %s', filename);
+        var filepath = $path.join(INI_ROOT, name);
+        console.log('filepath: %s', filepath);
+
+        var config = iniparser.parseSync(filepath);
+        console.log('config:', config);
+        return config;
+      }
+    }
+  }
+]);
