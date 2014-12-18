1ff68492659a04bedde3c7868021e9bb937e0daf
godric <godric@0x3f.fr>
2014-12-18 23:56:16 +0100

    aad: global shortcut

--- sources/services/zspin.js
+++ sources/services/zspin.js
@@ -5,6 +5,14 @@ app.factory('zspin', [
     var gui = require('nw.gui');
     var path = require('path');
 
+    // Create a shortcut.
+    var shortcut = new gui.Shortcut({key: 'Ctrl+4'});
+    gui.App.registerGlobalHotKey(shortcut);
+    shortcut.on('active', function() {
+      gui.Window.get().show();
+    });
+    //gui.App.unregisterGlobalHotKey(shortcut);
+
     return {
       // Set root paths
       dataPath: path.join(gui.App.dataPath, 'Zspin')
