e4497528688bd7262e6ff157776b98eab02bc580
godric <godric@0x3f.fr>
2015-01-06 00:52:05 +0100

    imp: background and position

--- sources/directives/theme.js
+++ sources/directives/theme.js
@@ -88,42 +88,37 @@ app.directive('themeartwork', [
         file: '=',
       },
       link: function(scope, el, attrs) {
-
+          console.log('hey', scope.name, scope.file);
         scope.onload = function() {
-          console.log('onload');
-          // var css = {};
-          // // Copy conf
-          // if (!scope.config) return;
-          // var conf = scope.config;
-          // // Try to get item natural dimesions
-          // var $el = $('.theme-entry#'+scope.name, el);
-          // if ($el.length) {
-          //   var width  = $el[0].naturalWidth;
-          //   var height = $el[0].naturalHeight;
-          // }
-          // // Config simili-parsing
-          // if (conf.w)
-          //   css.width = (conf.w||width||0);
-          // if (conf.w)
-          //   css.height = (conf.h||height||0);
-
-          // css.left = (conf.x-(css.width/2)) || 0;
-          // css.top = (conf.y-(css.height/2)) || 0;
-          // // console.log('done', css);
-          // // scope.style = css;
+          console.log('onload', scope.name, scope.file);
+          var css = {};
+          // Copy conf
+          if (!scope.config) { 
+            return {
+              width: '1024px',
+              height: '768px',
+            };
+          }
+          var conf = scope.config;
+          // Try to get item natural dimesions
+          var $el = $('.theme-entry#'+scope.name, el);
+          if ($el.length) {
+            var width  = $el[0].naturalWidth;
+            var height = $el[0].naturalHeight;
+          }
+          // Config simili-parsing
+          if (scope.name == 'background') {
+            css.width = '1024px';
+            css.height = '768px';
+          } else {
+            css.width = (conf.w||width||0);
+            css.height = (conf.h||height||0);
+          }
+          css.left = (conf.x-(css.width/2)) || 0;
+          css.top = (conf.y-(css.height/2)) || 0;
+          console.log('done', scope.name, css);
+          scope.style = css;
         };
-
-        // scope.$watch('path', function(path) {
-        //   scope.type = '';
-        //   if (!path) return;
-        //   var ext = scope.extname(path);
-        //   if (ext === 'swf')
-        //     scope.type = 'flash';
-        //   else
-        //     scope.type = 'image'
-        // });
-
-
 
       }
     };
