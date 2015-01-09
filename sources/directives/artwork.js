64276f63641fe726cf17b9b585c1115ad5ce5b5b
vik <v@42.am>
2015-01-09 20:10:21 +0100

    magnificent artwork.js

--- sources/directives/artwork.js
+++ sources/directives/artwork.js
@@ -11,12 +11,12 @@ app.directive('artwork', ['fs',
         file: '=',
       },
       link: function(scope, el, attrs) {
-
+        // TODO: what do we do here ?
         scope.size = {width: 0, height: 0};
-
 
         scope.$watch('file', function(file) {
 
+          // if file is a flash file
           if (file && file.type === 'swf') {
             SWFReader.read(file.path, function(err, swf) {
               console.log('swf loaded', err);
@@ -26,8 +26,9 @@ app.directive('artwork', ['fs',
             });
           }
 
+          // else
           if (file && file.type !== 'swf') {
-            var $el = $('.theme-entry-item', el)
+            var $el = $('.theme-entry-item', el);
             $el.on('load', function() {
               scope.$apply(function() {
                 scope.size = {
@@ -41,21 +42,26 @@ app.directive('artwork', ['fs',
 
         });
 
-
+        //  -  Set element's css rules
         scope.$watch('size', function() {
           console.log('onload', scope.name, scope.file);
+
           var css = {};
+
           // Copy conf
           if (!scope.config)
             return;
-          var conf = scope.config;
-          // Try to get item natural dimesions
-          // Config simili-parsing
-          css.width = (conf.w||scope.size.width||0);
-          css.height = (conf.h||scope.size.height||0);
-          css.left = (conf.x-(css.width/2)) || 0;
-          css.top = (conf.y-(css.height/2)) || 0;
 
+          var conf = scope.config;
+
+          // Try to get item natural dimensions
+          // Config simili-parsing
+          css.width = (conf.w || scope.size.width || 0);
+          css.height = (conf.h || scope.size.height || 0);
+          css.left = (conf.x - (css.width / 2)) || 0;
+          css.top = (conf.y - (css.height / 2)) || 0;
+
+          // TODO: set 1024/768 constants somewhere
           css.width = css.width * window.innerWidth / 1024;
           css.height = css.height * window.innerHeight / 768;
 
