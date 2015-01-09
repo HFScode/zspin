a30f028c4768b2ed0ee739165b03204b60473ff3
vik <v@42.am>
2015-01-09 20:16:43 +0100

    clearer fs.js

--- sources/services/fs.js
+++ sources/services/fs.js
@@ -9,10 +9,12 @@ app.factory('fs', ['$q',
     var mkdirp = require('mkdirp');
     var rimraf = require('rimraf');
 
+    //  -  defining services  -
     var service = {
       join: function() {
         return path.join.apply(path, arguments);
       },
+
       readdir: function() {
         var defer = $q.defer();
         wrapErrCallback(defer, fs, fs.readdir, arguments);
@@ -20,6 +22,7 @@ app.factory('fs', ['$q',
           return args[0];
         });
       },
+
       readFile: function() {
         var defer = $q.defer();
         wrapErrCallback(defer, fs, fs.readFile, arguments);
@@ -27,16 +30,19 @@ app.factory('fs', ['$q',
           return args[0];
         });
       },
+
       writeFile: function() {
         var defer = $q.defer();
         wrapErrCallback(defer, fs, fs.writeFile, arguments);
         return defer.promise;
       },
+
       mkdir: function() {
         var defer = $q.defer();
         wrapErrCallback(defer, null, mkdirp, arguments);
         return defer.promise;
       },
+
       exists: function() {
         var defer = $q.defer();
         wrapCallback(defer, fs, fs.exists, arguments);
@@ -44,11 +50,13 @@ app.factory('fs', ['$q',
           return args[0];
         });
       },
+
       stat: function() {
         var defer = $q.defer();
         wrapErrCallback(defer, fs, fs.stat, arguments);
         return defer.promise;
       },
+
       mktmpfile: function() {
         var defer = $q.defer();
         wrapErrCallback(defer, tmp, tmp.file, arguments);
@@ -56,6 +64,7 @@ app.factory('fs', ['$q',
           return {path: args[0], fd: args[1], clean: args[2]};
         });
       },
+
       mktmpdir: function() {
         var defer = $q.defer();
         wrapErrCallback(defer, tmp, tmp.dir, arguments);
@@ -63,11 +72,13 @@ app.factory('fs', ['$q',
           return {path: args[0], clean: args[2]};
         });
       },
+
       rmrf: function(path) {
         var defer = $q.defer();
         wrapErrCallback(defer, null, rimraf, arguments);
         return defer.promise;
       }
+
     };
     console.log('fs - ready');
     return service;
