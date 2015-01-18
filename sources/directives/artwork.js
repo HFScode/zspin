ae4a576b73ea97308c3f47b8bffcdda97a473fe4
godric <godric@0x3f.fr>
2015-01-18 15:23:45 +0100

    imp: artwork directive cleanup

--- sources/directives/artwork.js
+++ sources/directives/artwork.js
@@ -10,31 +10,36 @@ app.directive('artwork', ['$q', '$timeout', 'fs',
         name: '=',
         config: '=',
         file: '=',
-        overlay: '=', 
+        overlay: '=',
       },
       link: function(scope, el, attrs) {
 
         // Get the size of a .swf (w,h) by parsing the file
         function getFlashSize() {
           var d = $q.defer();
-          SWFReader.read(scope.file.path, function(err, swf) {
-            if (err) {
-              d.reject(err);
-            } else {
-              d.resolve(swf.frameSize);
-            }
+          var path = scope.file.path;
+          SWFReader.read(scope.file.path, function(err, res) {
+            if (err) { d.reject(err); }
+            else { d.resolve(res.frameSize); }
           });
           return d.promise;
         }
 
+        function getVideoSize() {
+          var d = $q.defer();
+          $timeout(d.resolve, 0);
+          return d.promise;
+        }
+
+
         // Get the size of an <img> by asking the browser
         function getImageSize() {
           var d = $q.defer();
-          var $el = $('.theme-entry-item', el);
-          $el.on('load', function() {
+          var $el = el.find('.theme-entry-item');
+          $el.on('load', function(e) {
             d.resolve({
-              width  : $el[0].naturalWidth,
-              height : $el[0].naturalHeight,
+              width  : e.target.naturalWidth,
+              height : e.target.naturalHeight,
             });
           });
           return d.promise;
@@ -42,12 +47,12 @@ app.directive('artwork', ['$q', '$timeout', 'fs',
 
         /*------------------------- Get Artwork Size ------------------------*/
 
-        // Size is unknown at this point
-        // scope.size = {width: 0, height: 0};
+        scope.size = null;
 
-        //  -  Set element's size
         function updateSize(size) {
-          scope.size = size;
+          console.log('topdelire', scope.name);
+          scope.size = size || {};
+          updateStyle();
         }
 
         scope.$watch('file', function(file) {
@@ -57,41 +62,54 @@ app.directive('artwork', ['$q', '$timeout', 'fs',
           // if file is a plain image file
           else if (file && file.type === 'png')
             getImageSize().then(updateSize);
-          else if (file && file.type === 'flv') {
-            $timeout(function() {
-              updateSize({width: 0, hieght: 0});
-            }, 100);
-          }
+          // if the image is a flash video
+          else if (file && file.type === 'flv')
+            getVideoSize().then(updateSize);
          });
 
         /*------------------------ Set Artwork Style ------------------------*/
 
-        scope.style = {};
+        scope.style = {display: 'none'};
 
-        //  -  Set element's css rules
+        // Conpute & Set element's css rules
         function updateStyle() {
-          scope.style = {};
-          // console.log('config', scope.name, scope.config);
-          // If we don't have size or config do nothing
+          scope.style = {display: 'none'};
+          console.log('>',  scope.name, scope.config, scope.size);
+          // If we miss either config or size, abort
           if (!scope.config || !scope.size)
-            return;
+            return ;
 
+          // Init variables
           var css = {};
           var conf = scope.config;
 
-          // Config simili-parsing
-          css.width = (conf.w || scope.size.width || 0);
+          // Original artork size might be overrided in config
+          // otherwise, use the native artwork size
+          css.width  = (conf.w || scope.size.width || 0);
           css.height = (conf.h || scope.size.height || 0);
+
+          // Position (x,y) is defined as the position of the center
+          // of the resized artwork relative to window (top,left)
+          // posX = x - (width / 2)
           css.left = (conf.x - (css.width / 2)) || 0;
           css.top = (conf.y - (css.height / 2)) || 0;
-          if (!!scope.overlay) {
-            css.left += ((css.width - scope.size.width) / 2) + parseFloat(conf.overlayoffsetx);
-            css.top  += ((css.height - scope.size.height) / 2) + parseFloat(conf.overlayoffsety);
+
+          // If the item is an overlay, the previously parsed config
+          // is its artorwk's and the actual size and position can be
+          // deduced from it.
+          if (scope.overlay === true) {
+            // By default, the overlay has its center aligned with the artwork's center
+            css.left = css.left + ((css.width - scope.size.width) / 2);
+            css.top  = css.top + ((css.height - scope.size.height) / 2);
+            // Apply overlay offsets if any
+            css.left += parseInt(conf.overlayoffsetx);
+            css.top += parseInt(conf.overlayoffsety);
+            // The overlay size is its native one
             css.width = (scope.size.width || 0);
             css.height = (scope.size.height || 0);
           }
 
-          // Scale from the original 1024 * 768 to windows size
+          // Scale from the Hyperspin's 1024 * 768 to zspin window size
           var W_RATIO = window.innerWidth / 1024;
           var H_RATIO = window.innerHeight / 768;
 
@@ -101,18 +119,11 @@ app.directive('artwork', ['$q', '$timeout', 'fs',
           css.left = css.left * W_RATIO;
 
           scope.style = css;
-          if (scope.name == 'video') {
-            console.log(conf);
-            console.log(css);
-          }
-
-
-          // console.log(scope.name, 'style=', css);
         }
 
         // Update styles when needed
         scope.$on('resize', updateStyle);
-        scope.$watch('size', updateStyle);
+        // scope.$watch('size', updateStyle);
         scope.$watch('config', updateStyle);
 
       }
