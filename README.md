8fc4ae96f8a62ee2bb6d91c2c481f33e172c6ae5
godric <godric@0x3f.fr>
2015-01-27 02:04:19 +0100

    imp: refacoring, better build & makefile

--- README.md
+++ README.md
@@ -7,20 +7,20 @@ Multi-OS Hyperspin drop in replacement.
 ### OSX
 
 ```bash
-brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup
-brew cask install node-webkit
-ln -s /opt/homebrew-cask/Caskroom/node-webkit/0.11.2/node-webkit-v0.11.2-osx-x64/node-webkit.app/Contents/MacOS/node-webkit node-webkit
-npm install -g gulp
-npm install -g bower
-npm install
-bower install
-cd public
-npm install
-cd ..
+# nodejs install (brew)
+$> brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup
+# nodejs install (macports)
+$> port install nodejs npm
+
+# install & build app
+$> make
+
+# (optional) watch source & build
+$> make watch
+
+# run app
+$> make run
+
+# make releases
+$> make release
 ```
-
-and then `gulp watch` and then `./node-webkit public`
-
-
-if installed with brew:
-`/opt/homebrew-cask/Caskroom/node-webkit/0.11.2/node-webkit-v0.11.2-osx-x64/node-webkit.app/Contents/MacOS/node-webkit public`
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var gulp      = require('gulp');
-var gu_concat = require('gulp-concat');
-var gu_uglify = require('gulp-uglify');
-var gu_minify = require('gulp-minify-css');
-var gu_sass   = require('gulp-sass');
-var gu_tpls   = require('gulp-angular-templatecache');
-var gu_lr     = require('gulp-livereload');
-
+var gulp       = require('gulp');
+var gu_concat  = require('gulp-concat');
+var gu_uglify  = require('gulp-uglify');
+var gu_minify  = require('gulp-minify-css');
+var gu_sass    = require('gulp-sass');
+var gu_tpls    = require('gulp-angular-templatecache');
+var gu_lr      = require('gulp-livereload');
+var gu_install = require('gulp-install');
 
 var vendors = {
   'scripts': [
@@ -35,19 +35,19 @@ gulp.task('vendors:scripts', function() {
   return gulp.src(vendors.scripts)
     .pipe(gu_concat('vendors.js'))
     .pipe(gu_uglify())
-    .pipe(gulp.dest('public/js'));
+    .pipe(gulp.dest('build/js'));
 });
 
 gulp.task('vendors:styles', function() {
   return gulp.src(vendors.styles)
     .pipe(gu_concat('vendors.css'))
     .pipe(gu_minify())
-    .pipe(gulp.dest('public/css'));
+    .pipe(gulp.dest('build/css'));
 });
 
 gulp.task('vendors:fonts', function() {
   return gulp.src(vendors.fonts)
-    .pipe(gulp.dest('public/fonts'));
+    .pipe(gulp.dest('build/fonts'));
 });
 
 gulp.task('vendors', ['vendors:scripts', 'vendors:styles' ,'vendors:fonts']);
@@ -55,22 +55,32 @@ gulp.task('vendors', ['vendors:scripts', 'vendors:styles' ,'vendors:fonts']);
 /************************************ App ************************************/
 
 var app = {
+  'statics': [
+    'app_statics/**/*',
+  ],
   'scripts': [
-    'sources/**/*.js',
-    'sources/index.js',
+    'app_sources/**/*.js',
+    'app_sources/index.js',
   ],
   'styles': [
-    'sources/styles/*.scss',
+    'app_sources/**/*.scss',
   ],
   'templates': [
-    'sources/templates/*.html',
+    'app_sources/templates/*.html',
+    'app_sources/**/*.html',
   ]
 };
+
+gulp.task('app:statics', function() {
+  return gulp.src(app.statics)
+    .pipe(gulp.dest('build'))
+    .pipe(gu_install());
+});
 
 gulp.task('app:scripts', function() {
   return gulp.src(app.scripts)
     .pipe(gu_concat('app.js'))
-    .pipe(gulp.dest('public/js'));
+    .pipe(gulp.dest('build/js'));
 });
 
 gulp.task('app:styles', function() {
@@ -81,27 +91,28 @@ gulp.task('app:styles', function() {
       sourceMap: 'sass'
     }))
     .pipe(gu_concat('app.css'))
-    .pipe(gulp.dest('public/css'));
+    .pipe(gulp.dest('build/css'));
 });
 
 gulp.task('app:templates', function() {
   return gulp.src(app.templates)
     .pipe(gu_tpls({standalone: true}))
     .pipe(gu_concat('app.tpls.js'))
-    .pipe(gulp.dest('public/js'));
+    .pipe(gulp.dest('build/js'));
 });
 
 
-gulp.task('app', ['app:scripts', 'app:styles', 'app:templates']);
+gulp.task('app', ['app:statics', 'app:scripts', 'app:styles', 'app:templates']);
 
 /*********************************** Watch ***********************************/
 
 gulp.task('watch', ['default'], function() {
   gu_lr.listen();
+  gulp.watch(app.statics,   ['app:statics']);
   gulp.watch(app.styles,    ['app:styles']);
   gulp.watch(app.scripts,   ['app:scripts']);
   gulp.watch(app.templates, ['app:templates']);
-  gulp.watch('public/**').on('change', gu_lr.changed);
+  gulp.watch('build/**').on('change', gu_lr.changed);
 });
 
 gulp.task('default', ['vendors', 'app']);
