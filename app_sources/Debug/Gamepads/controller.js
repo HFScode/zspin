adb390db5691a31c8bde8e98a9dcdae9a5f99fa6
godric <godric@0x3f.fr>
2015-02-05 00:43:38 +0100

    improvements on DebugGamepad controller

--- app_sources/Debug/Gamepads/controller.js
+++ app_sources/Debug/Gamepads/controller.js
@@ -3,9 +3,5 @@
 app.controller('DebugGamepadsCtrl', ['$scope', 'gamepads',
   function($scope, gamepads) {
     $scope.gamepads = gamepads.gamepads;
-
-    $scope.update = function() {
-      // noop
-    }
   }
 ]);
