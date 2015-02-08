f8c7037e23310339371f19ad92167ecfc2204736
godric <godric@0x3f.fr>
2015-02-08 22:24:09 +0100

    imp: better gamepad debug menu

--- app_sources/Debug/Gamepads/controller.js
+++ app_sources/Debug/Gamepads/controller.js
@@ -3,5 +3,19 @@
 app.controller('DebugGamepadsCtrl', ['$scope', 'gamepads',
   function($scope, gamepads) {
     $scope.gamepads = gamepads.gamepads;
+
+
+    $scope.log = {};
+    $scope.update = function(input) {
+      var gpdidx = input.gamepad;
+      var log = ['gpd #', gpdidx, ' ', input.combo, ':', input.value];
+
+      var slice = $scope.log[gpdidx] || [];
+      slice = slice.slice(-5);
+      slice.push(log.join(''));
+      $scope.log[gpdidx] = slice;
+      // if ($scope.log[gpdidx].length > 5)
+      //   $scope.log[gpdidx].shift();
+    };
   }
 ]);
