532cd271d884ce9cb72a34b972a761ae9e57327b
godric <godric@0x3f.fr>
2015-02-09 01:02:58 +0100

    add: menu debug controller

--- app_sources/Debug/Menus/controller.js
+++ app_sources/Debug/Menus/controller.js
@@ -1,7 +1,29 @@
 'use strict';
 
-app.controller('DebugMenusCtrl', ['$scope', 'gamepads',
-  function($scope, gamepads) {
-    $scope.gamepads = gamepads.gamepads;
+app.controller('DebugMenusCtrl', ['$scope', 'zspin', 'fs', 'menus',
+  function($scope, zspin, fs, menus) {
+    $scope.menuName = 'Main Menu';
+
+    $scope.menu = {};
+    $scope.settings = {};
+    $scope.databases = {};
+
+    fs.readdir(zspin.path('Settings')).then(function(dirs) {
+      $scope.options = dirs.map(function(dir) {
+        return dir.replace('.ini', '');
+      })
+    });
+
+    $scope.update = function() {
+      $scope.menu = menus($scope.menuName);
+      $scope.settings = {};
+      $scope.menu.settings().then(function(data) {
+        $scope.settings = data;
+      });
+      $scope.databases = {};
+      $scope.menu.databases().then(function(data) {
+        $scope.databases = data;
+      });
+    };
   }
 ]);
