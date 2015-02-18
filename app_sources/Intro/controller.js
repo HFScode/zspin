9c8af1447bc82c5d877d749c6e146a2229391d25
godric <godric@0x3f.fr>
2015-02-18 01:29:47 +0100

    poc: Intro controller

--- app_sources/Intro/controller.js
+++ app_sources/Intro/controller.js
@@ -1,7 +1,26 @@
 'use strict';
 
-app.controller('IntroCtrl', ['$scope', 'gamepads',
-  function($scope, gamepads) {
-    $scope.gamepads = gamepads.gamepads;
+app.controller('IntroCtrl', ['$scope', 'fs', 'zspin',
+  function($scope, fs, zspin) {
+
+    function basename(filename) {
+      return filename.toLowerCase().replace(/\..*?$/, '');
+    }
+    function tmp_path () {
+      return fs.join.bind(fs, $scope.path).apply(fs, arguments);
+    }
+
+    $scope.path = zspin.path('Media', 'Frontend', 'Video');
+    fs.readdir($scope.path).then(function(files) {
+      var videos = files.filter(function(file) {
+        return basename(file) === 'intro';
+      }).map(function(file) {
+        return tmp_path(file);
+      });
+      if (videos.length !== 0)
+        $scope.video = videos[0];
+    });
+
+    $scope.player = {};
   }
 ]);
