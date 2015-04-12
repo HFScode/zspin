583dfedc2dd596d090ae616efddeb32d1fccab37
godric <godric@0x3f.fr>
2015-04-12 19:17:45 +0200

    imp: move artwork logic into own service

--- app_sources/Artwork/directive.js
+++ app_sources/Artwork/directive.js
@@ -1,11 +1,9 @@
-app.directive('artwork', ['$q', '$timeout', 'fs',
-  function($q, $timeout, fs) {
+app.directive('artwork', ['$q', 'artworks',
+  function($q, artworks) {
     var SWFReader = require('swf-reader');
 
     return {
       restrict: 'E',
-      replace: true,
-      transclude: true,
       templateUrl: 'Artwork/template.html',
       scope: {
         src: '@',
@@ -14,85 +12,16 @@ app.directive('artwork', ['$q', '$timeout', 'fs',
       },
       link: function(scope, el, attrs) {
 
-        // Get the size of a .swf (w,h) by parsing the src
-        scope.getFlashSize = function() {
-          SWFReader.read(scope.src, function(err, res) {
-            scope.$apply(function() {
-              scope.naturalSize = res && res.frameSize;
-            });
-          });
-        };
-
-        // Get the size of an <img> by asking the browser
-        scope.getImageSize = function() {
-          var $el = el.find('.Artwork-item');
-          scope.naturalSize = {
-            width  : $el[0].naturalWidth,
-            height : $el[0].naturalHeight,
-          };
-        };
-
-        /*------------------------- Get Artwork Size ------------------------*/
-        scope.naturalSize = null;
-
-        function updateType() {
-          scope.type = fs.extname(scope.src||'');
-          scope.naturalSize = null;
-          if (scope.type === 'swf')
-            scope.getFlashSize();
-          if (scope.type === 'flv')
-            scope.naturalSize = {width: 200, height: 200};
-          // if (scope.type === 'png')
-          //   scope.getImageSize();
-        }
-
-        /*--------------------- Set Artwork Bounding Box --------------------*/
-
-        scope.box = {};
-        // Conpute & Set element's css rules
-        function updateBox() {
-          scope.box = {};
-          // If we miss either config or size, abort
-          if (!scope.config || !scope.naturalSize)
-            return ;
-
-          // Init variables
-          var box = {};
-          var conf = scope.config;
-
-          // Original artork size might be overrided in config
-          // otherwise, use the native artwork size
-          box.width  = (conf.w || scope.naturalSize.width || 0);
-          box.height = (conf.h || scope.naturalSize.height || 0);
-
-          // Position (x,y) is defined as the position of the center
-          // of the resized artwork relative to window (top,left)
-          // posX = x - (width / 2)
-          box.left = (conf.x - (box.width / 2)) || 0;
-          box.top = (conf.y - (box.height / 2)) || 0;
-
-          // If the item is an overlay, the previously parsed config
-          // is its artorwk's and the actual size and position can be
-          // deduced from it.
-         if (scope.overlay && scope.overlay !== 'false') {
-            // By default, the overlay has its center aligned with the artwork's center
-            box.left = box.left + ((box.width - scope.naturalSize.width) / 2);
-            box.top  = box.top + ((box.height - scope.naturalSize.height) / 2);
-            // Apply overlay offsets if any
-            box.left += parseInt(conf.overlayoffsetx);
-            box.top += parseInt(conf.overlayoffsety);
-            // The overlay size is its native one
-            box.width = (scope.naturalSize.width || 0);
-            box.height = (scope.naturalSize.height || 0);
-          }
-
-          scope.box = box;
-          updateStyle();
-        }
-
         /*---------------------- Set Artwork css Style ----------------------*/
-
         scope.style = {opacity: 0};
+
+        function computeBox() {
+          if (!scope.config || !scope.artwork)
+            return;
+          if (!scope.config.w && !scope.artwork.size)
+            return;
+          scope.box = artworks.computeBox(scope.artwork, scope.config, scope.overlay);
+        }
 
         function updateStyle() {
           var css = {};
@@ -108,12 +37,19 @@ app.directive('artwork', ['$q', '$timeout', 'fs',
           scope.style = css;
         }
 
-        // Update type detection
-        scope.$watch('src', updateType);
-        // Update styles when needed
-        scope.$watch('config', updateBox);
-        scope.$watch('naturalSize', updateBox);
-        // Rescale styles when needed
+        // Update artwork on source change
+        scope.$watch('src', function(src) {
+          console.log('oh', scope.src, scope.config)
+          scope.artwork = artworks(src);
+        });
+
+        // Update styles when config change
+        // or once the image size has fbeen found
+        scope.$watch('config', computeBox);
+        scope.$watch('artwork.size', computeBox);
+
+        // Recompute styles when needed
+        scope.$watch('box', updateStyle);
         scope.$on('resize', updateStyle);
 
       }
---
+++ app_sources/Artwork/service.js
@@ -0,0 +1,89 @@
+'use strict';
+
+app.factory('artworks', ['$q', 'qbind', 'fs',
+  function($q, qbind, fs) {
+    console.log('artworks - init');
+
+    var SWFReader = require('swf-reader');
+
+    function getFlashNaturalSize(src) {
+      return qbind.call(SWFReader, SWFReader.read, src)
+        .then(function(res) { return res.frameSize; });
+    }
+
+    function getImageNaturalSize(src) {
+      var $el = angular.element('<img src="'+src+'"/>');
+      return qbind.callSafe($el, $el.on, 'load').then(function(ev) {
+        return {width: $el[0].naturalWidth, height: $el[0].naturalHeight};
+      });
+    }
+
+
+    var service = function(src) {
+      var obj = {
+        src: src,
+        type: undefined,
+        size: {},
+      };
+
+      // Extract type from extname
+      obj.type = fs.extname(obj.src||'');
+
+      // Natural size method differ base on the type
+      var getSize = $q.when({width: 200, height: 200});
+      if (obj.type === 'png' || obj.type === 'jpg')
+        getSize = getImageNaturalSize(obj.src);
+      else if (obj.type === 'swf')
+        getSize = getFlashNaturalSize(obj.src);
+      else if (obj.type === 'flv')
+        getSize = $q.when({width: 200, height: 200});
+      getSize.then(function(size) {
+        obj.size = size;
+      });
+
+      obj.computeBox = service.computeBox.bind(null, obj);
+
+      return obj;
+    };
+
+    // Compute item bounding box base one config
+    service.computeBox = function(obj, conf, isOverlay) {
+      // If we miss either config or size, abort
+      if (!conf || !obj.size)
+        return {};
+
+      // Init variables
+      var box = {};
+
+      // Original artork size might be overrided in config
+      // otherwise, use the native artwork size
+      box.width  = (conf.w || obj.size.width || 0);
+      box.height = (conf.h || obj.size.height || 0);
+
+      // Position (x,y) is defined as the position of the center
+      // of the resized artwork relative to window (top,left)
+      // posX = x - (width / 2)
+      box.left = (conf.x - (box.width / 2)) || 0;
+      box.top = (conf.y - (box.height / 2)) || 0;
+
+      // If the item is an overlay, the previously parsed config
+      // is its artorwk's and the actual size and position can be
+      // deduced from it.
+     if (isOverlay && isOverlay !== 'false') {
+        // By default, the overlay has its center aligned with the artwork's center
+        box.left = box.left + ((box.width - obj.size.width) / 2);
+        box.top  = box.top + ((box.height - obj.size.height) / 2);
+        // Apply overlay offsets if any
+        box.left += parseInt(conf.overlayoffsetx);
+        box.top += parseInt(conf.overlayoffsety);
+        // The overlay size is its native one
+        box.width = (obj.size.width || 0);
+        box.height = (obj.size.height || 0);
+      }
+      return box;
+    };
+
+    console.log('artworks - ready');
+    return service;
+  }
+]);
