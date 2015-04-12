a1b420ae84d4443694b2ca5056de07d6df775f53
godric <godric@0x3f.fr>
2015-04-12 17:31:50 +0200

    imp: improve fs service

--- app_sources/Core/services/fs.js
+++ app_sources/Core/services/fs.js
@@ -6,49 +6,91 @@ app.factory('fs', ['$q',
     var fs = require('fs');
     var path = require('path');
     var tmp = require('tmp');
+    var glob = require('glob');
     var mkdirp = require('mkdirp');
     var rimraf = require('rimraf');
 
     //  -  Services for file system  -
     var service = {
 
+      /*************************** Custom filters ****************************/
+
       extname: function(filename) {
-        return filename.toLowerCase().replace(/^.*\./, '');
+        var str = filename.toLowerCase();
+        return str.replace(/^.*\./, '');
       },
 
       basename: function (filename) {
-        return filename.replace(/\..*?$/, '');
+        var str = path.basename(filename);
+        return str.replace(/\..*?$/, '');
       },
 
-      basenamelc: function (filename) {
-        return filename.toLowerCase().replace(/\..*?$/, '');
+      dirname: function (filename) {
+        return path.dirname(filename);
       },
+
+
+      // basenamelc: function (filename) {
+      //   var str = path.basename(filename);
+      //   str = str.toLowerCase();
+      //   return str.replace(/\..*?$/, '');
+      // },
 
       join: function() {
         return path.join.apply(path, arguments);
       },
 
-      readdir: function() {
+      /********************** General Purpose Proxies  **********************/
+
+      apply: function(funcname, args) {
         var defer = $q.defer();
-        wrapErrCallback(defer, fs, fs.readdir, arguments);
-        return defer.promise.then(function(args) {
-          return args[0];
-        });
+        // console.log('apply', funcname, args);
+        wrapErrCallback(defer, fs, fs[funcname], args);
+        return defer.promise;
+      },
+
+      call: function(funcname) {
+        var args = [].slice.call(arguments, 1);
+        // console.log('call', funcname, args);
+        return service.apply(funcname, args);
+      },
+
+      /*************************** Proxy functions ***************************/
+
+      readdir: function() {
+        function tr(args) { return args[0]; }
+        return service.apply('readdir', arguments).then(tr);
       },
 
       readFile: function() {
-        var defer = $q.defer();
-        wrapErrCallback(defer, fs, fs.readFile, arguments);
-        return defer.promise.then(function(args) {
-          return args[0];
-        });
+        function tr(args) { return args[0]; }
+        return service.apply('readFile', arguments).then(tr);
+      },
+
+      exists: function() {
+        function tr(args) { return args[0]; }
+        return service.apply('exists', arguments).then(tr);
+      },
+
+      F_OK: fs.F_OK, R_OK: fs.R_OK, W_OK: fs.W_OK, X_OK: fs.X_OK,
+      access: function() {
+        function tr(args) { return args[0]; }
+        return service.apply('access', arguments).then(tr);
       },
 
       writeFile: function() {
-        var defer = $q.defer();
-        wrapErrCallback(defer, fs, fs.writeFile, arguments);
-        return defer.promise;
+        return service.apply('readFile', arguments);
       },
+
+      /***************************** Other Tools *****************************/
+
+      glob: function() {
+        var defer = $q.defer();
+        function tr(args) { return args[0]; }
+        wrapErrCallback(defer, null, glob, arguments);
+        return defer.promise.then(tr);
+      },
+
 
       mkdir: function() {
         var defer = $q.defer();
@@ -56,34 +98,18 @@ app.factory('fs', ['$q',
         return defer.promise;
       },
 
-      exists: function() {
-        var defer = $q.defer();
-        wrapCallback(defer, fs, fs.exists, arguments);
-        return defer.promise.then(function(args) {
-          return args[0];
-        });
-      },
-
-      stat: function() {
-        var defer = $q.defer();
-        wrapErrCallback(defer, fs, fs.stat, arguments);
-        return defer.promise;
-      },
-
       mktmpfile: function() {
         var defer = $q.defer();
+        function tr(r) { return {path: r[0], fd: r[1], clean: r[2]}; }
         wrapErrCallback(defer, tmp, tmp.file, arguments);
-        return defer.promise.then(function(args) {
-          return {path: args[0], fd: args[1], clean: args[2]};
-        });
+        return defer.promise.then(tr);
       },
 
       mktmpdir: function() {
         var defer = $q.defer();
+        function tr(r) { return {path: r[0], clean: r[2]}; }
         wrapErrCallback(defer, tmp, tmp.dir, arguments);
-        return defer.promise.then(function(args) {
-          return {path: args[0], clean: args[2]};
-        });
+        return defer.promise.then(tr);
       },
 
       rmrf: function(path) {
