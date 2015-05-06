2bfb6bb1137b084677413fbd4ec7d6e9802ab4c5
Romain Gay <v@42.am>
2015-05-06 19:27:15 +0200

    fix field update in settings

--- app_sources/Settings/controller.js
+++ app_sources/Settings/controller.js
@@ -50,6 +50,7 @@ app.controller('SettingsCtrl', ['$scope', 'DOMKeyboard', 'gamepads', 'settings',
 
     $scope.updatePath = function() {
       $scope.settings[this.name] = this.value;
+      $scope.$apply();
     }
 
   }
