dc108a8169aeb6e2a341b59e0d5f7e6dadf4447c
Godric <godric.com@gmail.com>
2015-05-11 23:51:17 +0200

    imp: fixe fs service dependency in Settings/service

--- app_sources/Settings/service.js
+++ app_sources/Settings/service.js
@@ -3,11 +3,16 @@
 app.factory('settings', [
   function () {
     console.log('settings - init');
-
-    var fs = require('fs');
-    var path = require('path');
-    var gui = require('nw.gui');
     var service = {};
+
+    var $fs = require('fs');
+    var $path = require('path');
+    var gui = require('nw.gui');
+
+    // Path for the app datas
+    // for example in osx: ~/Library/Application Support/zspin/Zspin
+    var dataPath = $path.join(gui.App.dataPath, 'Zspin');
+    var settingsPath = $path.join(dataPath, 'Settings.json')
 
     service.$obj = {
       binds: {
@@ -19,39 +24,24 @@ app.factory('settings', [
         'enter': {},
         'back': {}
       },
-      hsPath: '',
+      hsPath: dataPath,
       launcherPath: '',
       launcherParams: ''
     };
 
-    // Getting path of executable
-    var binaryPath = process.cwd(); // FIXME test this in windows/linux
-    if (process.platform == 'darwin') {
-        // avoids to get the binary app.nw when released in a .app mac package
-        binaryPath = binaryPath.replace(/\/[^\/]+\.app\/.*$/g, '');
-    }
-
-    // generates a path relative to the zspin binary path
-    service.binaryPath = function() {
-      var args = [].slice.call(arguments, 0);
-      return path.join.apply(null, [binaryPath].concat(args));
-    }
-
-    // generates a path relative to the 'data' path of zspin
-    // for example in osx: ~/Library/Application Support/zspin/Zspin
-    // FIXME: use the hyperspin.exe path in settings for configuring this (if available)
-    var dataPath = path.join(gui.App.dataPath, 'Zspin');
-
+    // Generates a path relative to the 'hsPath' path setting
     service.dataPath = function() {
       var args = [].slice.call(arguments, 0);
-      return path.join.apply(null, [dataPath].concat(args));
+      return $path.join.apply(null, [dataPath].concat(args));
     };
 
-    var settingsPath = service.dataPath('Settings.json');
-    // Loads Settings.json next to binary if this file exists
-    if (fs.existsSync(service.binaryPath('Settings.json'))) {
-      settingsPath = service.binaryPath('Settings.json');
-    }
+    // Generates a path relative to the hyperspin.exe path (user-settable)
+    service.hsPath = function() {
+      var hsPath = service.$obj.hsPath;
+      var args = [].slice.call(arguments, 0);
+      return $path.join.apply(null, [hsPath].concat(args));
+    };
+
 
     // Blocking settings "write"
     service.write = function() {
@@ -59,12 +49,12 @@ app.factory('settings', [
       for (var idx in data.binds['home'])
         data.binds['home'][idx].global = true;
       data = JSON.stringify(data, null, 2);
-      return fs.writeFileSync(settingsPath, data, 'utf8');
+      return $fs.writeFileSync(settingsPath, data, 'utf8');
     };
 
     // Blocking settings "load"
     service.load = function() {
-      var data = fs.readFileSync(settingsPath, 'utf8');
+      var data = $fs.readFileSync(settingsPath, 'utf8');
       try {
         var obj = JSON.parse(data);
         angular.extend(service.$obj, obj);
@@ -73,9 +63,28 @@ app.factory('settings', [
       }
     };
 
-    // Blocking settings "init" (find or create)
+    // Settings setup 
+    //
+    // This has to be blocking because we can't do anything without
+    // the settings. Blocking coveniently stops angular from loading
+    // before everything is ready.
+
+    // Path of the app exetubale 
+    var binaryPath = $path.dirname(process.execPath); // FIXME test this in windows/linux
+    if (process.platform == 'darwin') {
+      binaryPath = binaryPath.replace(new RegExp('/[^/]+\.app/.*$'), '');
+    }
+
+    // Override settingsPath with binaryPath if it contains Settings.json
     try {
-      fs.accessSync(settingsPath, fs.F_OK);
+      var _path = $path.join(binaryPath, 'Settings.json')
+      $fs.accessSync(_path, $fs.F_OK | $fs.R_OR);
+      settingsPath = _path;
+    } catch (e) {}
+
+    // Write defaults settings if there is no Settings.json to load
+    try {
+      $fs.accessSync(settingsPath, $fs.F_OK);
       console.log('Settings file: ' + settingsPath);
     } catch (e) {
       console.log('No settings file ! Creating ' + settingsPath);
@@ -84,12 +93,6 @@ app.factory('settings', [
 
     // Load curent values
     service.load();
-
-    // Generates a path relative to the hyperspin.exe path (user-settable)
-    service.hsPath = function() {
-      var args = [].slice.call(arguments, 0);
-      return path.join.apply(null, [path.dirname(service.$obj.hsPath)].concat(args));
-    };
 
     console.log('settings - ready');
     return service;
