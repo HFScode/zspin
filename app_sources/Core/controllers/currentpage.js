81b166cbfb6a01297ac5a05fda2dee6bfb137775
Romain Gay <v@42.am>
2016-04-02 00:27:01 +0200

    improve url hashing

--- app_sources/Core/controllers/currentpage.js
+++ app_sources/Core/controllers/currentpage.js
@@ -3,7 +3,13 @@
 app.controller('CurrentPage', ['$scope', '$location',
   function($scope, $location) {
     $scope.$on('$locationChangeSuccess', function (event, newLoc, oldLoc) {
-      $scope.currentPath = window.location.hash.replace(/\//g, ' ').replace(/[^a-z ]/ig, '');
+      // generate some class names for <body> tag, based on first 2 url hashes
+      var hashes = window.location.hash
+        .replace(/\//g, ' ')
+        .replace(/[^a-z ]/ig, '')
+        .trim()
+        .split(' ');
+      $scope.currentPath = hashes.slice(0, 2).join(' ');
     });
   }
 ]);
