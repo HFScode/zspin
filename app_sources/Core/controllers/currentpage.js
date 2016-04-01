4f57edf5e18e30b9ed3f3b07e03319282bd136a8
Romain Gay <v@42.am>
2016-04-01 21:17:39 +0200

    white background on settings and debug

--- app_sources/Core/controllers/currentpage.js
+++ app_sources/Core/controllers/currentpage.js
@@ -3,7 +3,7 @@
 app.controller('CurrentPage', ['$scope', '$location',
   function($scope, $location) {
     $scope.$on('$locationChangeSuccess', function (event, newLoc, oldLoc) {
-      $scope.currentPath = window.location.hash.replace(/[^a-z]/ig, '');
+      $scope.currentPath = window.location.hash.replace(/\//g, ' ').replace(/[^a-z ]/ig, '');
     });
   }
 ]);
