1d94108ffce4b18660d2de81c5feedf8f5ddc13e
karkinge <karkinge@gmail.com>
2016-09-28 14:39:09 +0200

    No cursor unless in debug mode. Cursor cannot be hidden over flash video players (intro,items).

--- app_sources/Core/controllers/currentpage.js
+++ app_sources/Core/controllers/currentpage.js
@@ -1,7 +1,7 @@
 'use strict';
 
-app.controller('CurrentPage', ['$scope', '$location',
-  function($scope, $location) {
+app.controller('CurrentPage', ['$scope', '$location', 'zspin',
+  function($scope, $location, zspin) {
     $scope.$on('$locationChangeSuccess', function (event, newLoc, oldLoc) {
       // generate some class names for <body> tag, based on first 2 url hashes
       var hashes = window.location.hash
@@ -10,6 +10,8 @@ app.controller('CurrentPage', ['$scope', '$location',
         .trim()
         .split(' ');
       $scope.currentPath = hashes.slice(0, 2).join(' ');
+      $scope.zspinOptions = zspin.options;
     });
+
   }
 ]);
